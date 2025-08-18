cb8015eb96b1b765750b07ba623da6484d0f4a9a
Branch Hill <branchhill97@gmail.com>
2025-08-17 22:02:07 -0500

    working on web api

---
+++ WebApi/src/Kernel/Entity.cs
@@ -0,0 +1,16 @@
+﻿namespace Kernel;
+
+public abstract class Entity
+{
+    protected Entity(Guid id)
+    {
+        Id = id;
+    }
+
+    protected Entity()
+    {
+    }
+
+    public Guid Id { get; init; }
+
+}
